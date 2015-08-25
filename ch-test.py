825a45fd70dbbae45f9ee3c68cd1d312d90f6ff7
onenote <ivicoco@gmail.com>
2015-08-25 16:02:03 +0800

    test file adding

---
+++ ch-test.py
@@ -0,0 +1,10 @@
+#! /usr/local/bin/ python
+# -*- coding: utf-8 -*-
+
+print u'中文测试'
+print '------------------------------'
+
+name = raw_input(u'请输入一个中文名字：'.encode('gbk')).decode('gbk')
+print '--------%s' % name
+print 'The name is', name
+print u'输入名称为: %s' % (name)
