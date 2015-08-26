c96ad41fe5f2ca7f4c7a9774e16842554fc46933
onenote <ivicoco@gmail.com>
2015-08-26 11:51:38 +0800

    add hello

---
+++ hello.py
@@ -0,0 +1,8 @@
+#! /usr/local/bin/ python
+# -*- coding: utf-8 -*-
+
+name = 'hello'
+print '-----------------START------------------'
+print '--------%s' % name
+print 'The name is', name
+print '-----------------END------------------'
