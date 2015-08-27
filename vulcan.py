f60edce0e3663e1bb9046b7919f1569bb36daf24
onenote <ivicoco@gmail.com>
2015-08-27 17:11:43 +0800

    add feature vulcan

---
+++ vulcan.py
@@ -0,0 +1,8 @@
+#! /usr/local/bin/ python
+# -*- coding: utf-8 -*-
+
+name = 'vulcan'
+print '-----------------START------------------'
+print '--------%s' % name
+print 'The name is', name
+print '-----------------END------------------'
